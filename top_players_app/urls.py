f514a4696861adc55aa984eb3ab85cc9278b59b6
Gleb Shmarov <gshmarov@parallels.com>
2020-11-23 01:36:26 +0300

    Add routing

---
+++ top_players_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from .views import PlayerViewSet
+
+player_all = PlayerViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+player_concrete = PlayerViewSet.as_view({
+    'get': 'retrieve',
+    'patch': 'partial_update',
+})
+
+urlpatterns = [
+     path('players/', player_all, name='player-all'),
+     path('players/<int:pk>/', player_concrete, name='player-concrete'),
+]
