5c022c56584bdbed6b34dfe5052b9cffc90a5778
Gleb Shmarov <gshmarov@parallels.com>
2020-11-23 01:24:56 +0300

    Add view for Player.

--- top_players_app/views.py
+++ top_players_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Player
+from .serializers import PlayerSerializer
 
-# Create your views here.
+class PlayerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows various operations over players.
+    """
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+    def get_queryset(self):
+        return Player.objects.all().order_by('-highscore')
