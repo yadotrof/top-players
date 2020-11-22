ec83b8c9f4666b95fd88ec04bdf841a67ce6661e
Gleb Shmarov <gshmarov@parallels.com>
2020-11-23 01:22:14 +0300

    Add serializer for Player.

---
+++ top_players_app/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Player
+
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['id', 'name', 'highscore', 'position']
